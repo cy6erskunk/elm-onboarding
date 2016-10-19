43f13b71364564c5a5a75a6d3b41df05df7017ba
Igor Shevchenko <cyberskunk@gmail.com>
2016-10-19 23:37:54 +0300

    add text input

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # elm-onboarding
 Just my personal elm onboarding repo
+
+Just run `elm-reactor` on open a browser proceded link (http://localhost:8000 by default)
--- index.elm
+++ index.elm
@@ -1,40 +1,52 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, Attribute, button, div, input, text)
 import Html.App as App
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import String
 
 main =
   App.beginnerProgram { model = model, view = view, update = update }
 
 -- Model
 
-type alias Model = Int
+type alias Model =
+  { content : String
+  , counter : Int
+  }
 model: Model
 model =
-  0
+  { counter = 0, content = "" }
 
 -- Update
 
-type Msg = Increment | Decrement | Reset
+type Msg = Increment | Decrement | Reset | Change String
 
 update: Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { model | counter = model.counter + 1 }
 
     Decrement ->
-      model - 1
+      { model | counter = model.counter - 1 }
 
     Reset ->
-      0
+      { model | counter = 0 }
+
+    Change newContent ->
+      { model | content = newContent }
 
 -- View
 
 view: Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Reset ] [ text "reset"]
-    , div [] [ text (toString model) ]
-    , button [ onClick Decrement ] [ text "-"]
-    , button [ onClick Increment ] [ text "+"]
+    [ button [ onClick Reset ] [ text "reset" ]
+    , div [] [ text (toString model.counter) ]
+    , button [ onClick Decrement ] [ text "-" ]
+    , button [ onClick Increment ] [ text "+" ]
+    , div []
+      [ input [ placeholder "Enter text to reverse", onInput Change ] []
+      , div [] [ text (String.reverse model.content) ]
+      ]
     ]
