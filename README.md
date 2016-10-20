fb937337827125f76114778f27a23008b1485f51
Igor Shevchenko <cyberskunk@gmail.com>
2016-10-20 11:33:11 +0300

    do some more basic html experimenting

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Just my personal elm onboarding repo
 
 Just run `elm-reactor` on open a browser proceded link (http://localhost:8000 by default)
+// 
--- index.elm
+++ index.elm
@@ -1,9 +1,10 @@
-import Html exposing (Html, Attribute, button, div, input, text)
+import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onClick, onInput, onCheck)
 import String
 
+main : Program Never
 main =
   App.beginnerProgram { model = model, view = view, update = update }
 
@@ -12,14 +13,32 @@ main =
 type alias Model =
   { content : String
   , counter : Int
+  , name : String
+  , password : String
+  , passwordAgain : String
+  , isRevealed: Bool
   }
 model: Model
 model =
-  { counter = 0, content = "" }
+  { counter = 0
+  , content = "" 
+  , name = ""
+  , password = ""
+  , passwordAgain = ""
+  , isRevealed = False 
+  }
 
 -- Update
 
-type Msg = Increment | Decrement | Reset | Change String
+type Msg 
+  = Increment
+  | Decrement
+  | Reset
+  | Change String
+  | Name String
+  | Password String
+  | PasswordAgain String
+  | Reveal Bool
 
 update: Msg -> Model -> Model
 update msg model =
@@ -36,17 +55,53 @@ update msg model =
     Change newContent ->
       { model | content = newContent }
 
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+    
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+    Reveal doReveal ->
+      { model | isRevealed = doReveal }
+
 -- View
 
 view: Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Reset ] [ text "reset" ]
-    , div [] [ text (toString model.counter) ]
-    , button [ onClick Decrement ] [ text "-" ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ div []
+      [ button [ onClick Reset ] [ text "reset" ]
+      , div [] [ text (toString model.counter) ]
+      , button [ onClick Decrement ] [ text "-" ]
+      , button [ onClick Increment ] [ text "+" ]  
+      ]
     , div []
       [ input [ placeholder "Enter text to reverse", onInput Change ] []
       , div [] [ text (String.reverse model.content) ]
       ]
+    , div []
+      [ input [ type' "text", placeholder "Name", onInput Name ] []
+      , input [ type' (if model.isRevealed then "text" else "password"), placeholder "Password", onInput Password ] []
+      , input [ type' (if model.isRevealed then "text" else "password"), placeholder "Password Again", onInput PasswordAgain ] []
+      , input [ type' "checkbox", onCheck Reveal ] []
+      , text "reveal everything!"
+      ]
+    , viewValidation model
     ]
+
+viewValidation : Model -> Html Msg
+viewValidation model =
+  let
+    (color, message) =
+      if model.password == model.passwordAgain then
+        if (String.length model.password < 4) then
+          ("salmon", "so weak, sol languid...")
+        else
+          ("green", "OK")
+      else
+        ("red", "Are you a terrorist or what?")
+  in
+    div [ style [("color", color)] ] [ text message ]
