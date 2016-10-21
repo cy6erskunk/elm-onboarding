8d93df5fc857a114449ce420c4f328cedc05ac64
Igor Shevchenko <cyberskunk@gmail.com>
2016-10-21 16:20:12 +0300

    try an action

--- index.elm
+++ index.elm
@@ -3,10 +3,15 @@ import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput, onCheck)
 import String
+import Random
 
 main : Program Never
-main =
-  App.beginnerProgram { model = model, view = view, update = update }
+main = App.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
 
 -- Model
 
@@ -16,7 +21,8 @@ type alias Model =
   , name : String
   , password : String
   , passwordAgain : String
-  , isRevealed: Bool
+  , isRevealed : Bool
+  , dieFace : Int
   }
 model: Model
 model =
@@ -25,7 +31,8 @@ model =
   , name = ""
   , password = ""
   , passwordAgain = ""
-  , isRevealed = False 
+  , isRevealed = False
+  , dieFace = 1
   }
 
 -- Update
@@ -39,58 +46,80 @@ type Msg
   | Password String
   | PasswordAgain String
   | Reveal Bool
+  | Roll
+  | NewFace Int
 
-update: Msg -> Model -> Model
+update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      { model | counter = model.counter + 1 }
+      ({ model | counter = model.counter + 1 }, Cmd.none)
 
     Decrement ->
-      { model | counter = model.counter - 1 }
+      ({ model | counter = model.counter - 1 }, Cmd.none)
 
     Reset ->
-      { model | counter = 0 }
+      ({ model | counter = 0 }, Cmd.none)
 
     Change newContent ->
-      { model | content = newContent }
+      ({ model | content = newContent }, Cmd.none)
 
     Name name ->
-      { model | name = name }
+      ({ model | name = name }, Cmd.none)
 
     Password password ->
-      { model | password = password }
+      ({ model | password = password }, Cmd.none)
     
     PasswordAgain password ->
-      { model | passwordAgain = password }
+      ({ model | passwordAgain = password }, Cmd.none)
 
     Reveal doReveal ->
-      { model | isRevealed = doReveal }
+      ({ model | isRevealed = doReveal }, Cmd.none)
+
+    Roll ->
+      (model, Random.generate NewFace (Random.int 1 6))
+
+    NewFace newFace ->
+      ({model | dieFace = newFace }, Cmd.none)
 
 -- View
 
 view: Model -> Html Msg
 view model =
   div []
-    [ div []
+    [ viewCounter model
+    , viewInputWithReverse model
+    , viewLoginPassword model
+    , viewValidation model
+    , viewDice model
+    ]
+
+
+viewCounter: Model -> Html Msg
+viewCounter model =
+  div []
       [ button [ onClick Reset ] [ text "reset" ]
       , div [] [ text (toString model.counter) ]
       , button [ onClick Decrement ] [ text "-" ]
       , button [ onClick Increment ] [ text "+" ]  
       ]
-    , div []
+
+viewInputWithReverse: Model -> Html Msg
+viewInputWithReverse model =
+  div []
       [ input [ placeholder "Enter text to reverse", onInput Change ] []
       , div [] [ text (String.reverse model.content) ]
       ]
-    , div []
+
+viewLoginPassword: Model -> Html Msg
+viewLoginPassword model =
+  div []
       [ input [ type' "text", placeholder "Name", onInput Name ] []
       , input [ type' (if model.isRevealed then "text" else "password"), placeholder "Password", onInput Password ] []
       , input [ type' (if model.isRevealed then "text" else "password"), placeholder "Password Again", onInput PasswordAgain ] []
       , input [ type' "checkbox", onCheck Reveal ] []
       , text "reveal everything!"
       ]
-    , viewValidation model
-    ]
 
 viewValidation : Model -> Html Msg
 viewValidation model =
@@ -105,3 +134,22 @@ viewValidation model =
         ("red", "Are you a terrorist or what?")
   in
     div [ style [("color", color)] ] [ text message ]
+
+viewDice : Model -> Html Msg
+viewDice model =
+  div []
+    [ h1 [] [ text (toString model.dieFace) ]
+    , button [ onClick Roll ] [ text "Roll!"]
+    ]
+
+-- -- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model = 
+  Sub.none
+
+-- INIT
+
+init : (Model, Cmd Msg)
+init =
+  (model, Cmd.none)
