e4accd839c9ab618ce99ba388369bc7b446bc516
Igor Shevchenko <cyberskunk@gmail.com>
2016-10-24 21:58:36 +0300

    use custom header in request, display some output

--- index.elm
+++ index.elm
@@ -4,7 +4,6 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput, onCheck)
 import String
 import Http
-import Random
 import Json.Decode as Json
 import Task
 import Svg exposing (..)
@@ -22,8 +21,7 @@ main = App.program
 -- Model
 
 type alias Model =
-  { content : String
-  , counter : Int
+  { counter : Int
   , name : String
   , password : String
   , passwordAgain : String
@@ -31,7 +29,9 @@ type alias Model =
   , dieFace : Int
   , url : String
   , reqError : String
+  , someResponse : String
   , time : Time
+  , startTime : Time
   }
 
 -- Update
@@ -77,13 +77,16 @@ update msg model =
         Http.UnexpectedPayload s ->
           ({ model | reqError = "UnexpectedPayload: " ++ s }, Cmd.none)
         Http.BadResponse code str ->
-          ({ model | reqError = (toString code) ++ str }, Cmd.none)
+          ({ model | reqError = (toString code) ++ ": " ++ str }, Cmd.none)
 
-    FetchSucceed _ ->
-      (model, Cmd.none)
+    FetchSucceed str ->
+      ({ model | someResponse = str, reqError = "" }, Cmd.none)
 
     Tick newTime ->
-      ({ model| time = newTime }, Cmd.none)
+      ({ model
+      | startTime = if model.startTime == 0 then newTime else model.startTime
+      , time = if model.startTime == 0 then 1 * Time.second else newTime - model.startTime + 1 * Time.second
+      }, Cmd.none)
 
 -- View
 
@@ -101,7 +104,7 @@ viewClock : Model -> Html Msg
 viewClock model =
   let
     angle =
-      turns (Time.inMinutes model.time)
+      turns (Time.inMinutes model.time - 1/4)
 
     handX =
       toString (50 + 40 * cos angle)
@@ -109,9 +112,10 @@ viewClock model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    Svg.svg [ viewBox "0 0 100 100", Svg.Attributes.width "300px"]
+    Svg.svg [ viewBox "0 0 100 100", Svg.Attributes.width "100px"]
       [ circle [ cx "50", cy "50", r "45", fill "#0b79ce" ] []
       , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+      , Svg.text' [ x "25", y "45", fontSize "8px" ] [ Svg.text (toString <| model.time) ]
       ]
 
 viewLoginPassword: Model -> Html Msg
@@ -145,6 +149,7 @@ viewHttpReq model =
     , Html.text (toString model.url)
     , button [ onClick SendRequest ] [ Html.text "Go!" ]
     , div [] [ Html.text model.reqError ]
+    , div [] [ Html.text model.someResponse ]
     ]
 
 -- SUBSCRIPTIONS
@@ -157,13 +162,20 @@ subscriptions model =
 
 init : (Model, Cmd Msg)
 init =
-  (Model "xxx" 0 "" "" "" False 1 "http://localhost/" "" 0, Cmd.none)
+  (Model 0 "" "" "" False 1 "http://localhost/" "" "" 0 0, Cmd.none)
 
 -- HTTP
 fetchData : String -> Cmd Msg
 fetchData url =
-  Task.perform FetchFail FetchSucceed (Http.get decodeResponseJson url)
+-- Http.send : Settings -> Request -> Task RawError Response
+-- fromJson : Decoder a -> Task RawError Response -> Task Error a
+  Task.perform FetchFail FetchSucceed <| Http.fromJson decodeResponseJson <| Http.send Http.defaultSettings {
+    verb = "GET"
+  , headers = [("Accept", "application/json")]
+  , url = url
+  , body = Http.empty
+  }
 
 decodeResponseJson : Json.Decoder String
 decodeResponseJson =
-  Json.at [ "id", "name"] Json.string
+  Json.at [ "href"] Json.string
